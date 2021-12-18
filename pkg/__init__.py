46707ea6266a32c6cddb2c94edc8a0fa87159a0a
u6013779 <u6013779@utah.edu>
2021-12-17 22:00:59 -0700

    redo

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -2,4 +2,10 @@ print(f'Invoking __init__.py for {__name__}')
 A = ['quux', 'corge', 'grault']
 
 print(f'Invoking __init__.py for {__name__}')
-import pkg.mod1, pkg.mod2
+
+__all__ = [
+    'mod1',
+    'mod2',
+    'mod3',
+    'mod4',
+]
