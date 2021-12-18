bad20ae8da09c22eb398c25133a9dd0475edc94e
u6013779 <u6013779@utah.edu>
2021-12-17 22:05:03 -0700

    redo

--- mod.py
+++ mod.py
@@ -4,12 +4,14 @@ def foo(arg):
     print (f'arg={arg}')
 class Foo:
     pass
-
-print(s)
-print(a)
-foo('quux')
-x = Foo()
-print(x)
+if (__name__ == '__main__'):
+    print('Exectuing as a standalone script')
+    print(s)
+    print(a)
+    foo('quux')
+    x = Foo()
+    print(x)
 
 a = [100, 200, 300]
 print('a=', a)
+
